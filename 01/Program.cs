07d9ddcb378ad5a112fb3ae434924ed670d9dcc8
UlfWallin <herr.wallin@gmail.com>
2023-12-02 11:49:28 +0100

    first commit

---
+++ 01/Program.cs
@@ -0,0 +1,34 @@
+﻿var sum = 0;
+var partTwo = true;
+const string path = "input.txt";
+using var reader = new StreamReader(path);
+while (reader.Peek() >= 0)
+{
+    var line = reader.ReadLine() ?? "";
+    if (partTwo)
+        line = TextToDigits(line);
+
+    var numbers = ExtractNumbers(line);
+
+    var first = numbers[0];
+    var last = numbers[^1];
+    sum += first * 10 + last;
+}
+
+Console.WriteLine(sum);
+
+static int[] ExtractNumbers(string inputString) => (from c in inputString.ToCharArray()
+                                                    where Char.IsNumber(c)
+                                                    select int.Parse(c.ToString())).ToArray();
+
+static string TextToDigits(string input) => input
+        .Replace("one", "o1e")
+        .Replace("two", "t2o")
+        .Replace("three", "t3e")
+        .Replace("four", "4")
+        .Replace("five", "5e")
+        .Replace("six", "6")
+        .Replace("seven", "7n")
+        .Replace("eight", "e8t")
+        .Replace("nine", "n9e")
+        .Replace("zero", "0o");
---
+++ 02/Program.cs
@@ -0,0 +1,41 @@
+﻿var gameIdSums = 0;
+var gamePower = 0;
+using var reader = new StreamReader("input.txt");
+while (reader.Peek() >= 0)
+{
+    var line = reader.ReadLine() ?? "";
+    var gameId = line.Split(':')[0][5..];
+    var items = line.Split(':')[1].Split(';');
+    var gameItems = new Dictionary<string, int>();
+    foreach (var item in items)
+    {
+        var sets = item.Split(',');
+        foreach (var set in sets)
+        {
+            var colour = set.Trim().Split(' ')[1];
+            var count = int.Parse(set.Trim().Split(' ')[0]);
+            if (gameItems.ContainsKey(colour))
+            {
+                gameItems[colour] = Math.Max(gameItems[colour], count);
+            }
+            else
+            {
+                gameItems.Add(colour, count);
+            }
+        }
+    }
+    gameItems.TryGetValue("red", out int red);
+    gameItems.TryGetValue("green", out int green);
+    gameItems.TryGetValue("blue", out int blue);
+
+    gamePower += red * green * blue;
+
+    // only 12 red cubes, 13 green cubes, and 14 blue cubes?
+    if (red <= 12 && green <= 13 && blue <= 14)
+    {
+        gameIdSums += int.Parse(gameId);
+    }
+}
+
+Console.WriteLine($"Part one: {gameIdSums}");
+Console.WriteLine($"Part two: {gamePower}");
