1f3d26a07734059c9a015ef3b7832b50adea39f1
UlfWallin <herr.wallin@gmail.com>
2023-12-04 19:10:54 +0100

    Day 3, part two

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,7 +1,12 @@
 ﻿using System.Text.RegularExpressions;
+using System.Linq;
 
 int sum = 0;
+int sumRatio = 0;
 List<string> lines = [];
+Regex regex = new Regex("\\*");
+Regex numRegex = new Regex("\\d+");
+
 using var reader = new StreamReader("input.txt");
 while (reader.Peek() >= 0)
 {
@@ -14,9 +19,29 @@ lines.Add(new string('.', lines[0].Length));
 
 for (int il = 0; il < lines.Count - 2; il++)
 {
+    var matches = regex.Matches(lines[il + 1]);
+    var numsTop = numRegex.Matches(lines[il]);
+    var numsBottom = numRegex.Matches(lines[il + 2]);
+    var numsCenter = numRegex.Matches(lines[il + 1]);
+    foreach(Match match in matches)
+    {
+        List<int> adjacent = [];
+        AddAdjacents(numsTop, match, adjacent);
+        AddAdjacents(numsBottom, match, adjacent);
+        AddAdjacents(numsCenter, match, adjacent);
+
+        if (adjacent.Count == 2)
+        {
+            var ratio = adjacent[0] * adjacent[1];
+            sumRatio += ratio;
+        }
+    }
+
     sum += GetEngineParts(lines[il], lines[il + 1], lines[il + 2]).Sum();
 }
+
 Console.WriteLine(sum);
+Console.WriteLine(sumRatio);
 
 static int[] GetEngineParts(string top, string middle, string bottom)
 {
@@ -27,10 +52,6 @@ static int[] GetEngineParts(string top, string middle, string bottom)
     {
         var left = Math.Max(match.Index - 1, 0);
         var right = Math.Min(match.Index + match.Length + 1, middle.Length - 1);
-
-        Console.WriteLine(top.Substring(left, right - left));
-        Console.WriteLine(middle.Substring(left, right - left));
-        Console.WriteLine(bottom.Substring(left, right - left));
 
         bool adj = false;
 
@@ -47,10 +68,6 @@ static int[] GetEngineParts(string top, string middle, string bottom)
             adj = true;
         }
 
-        var adjT = IsSymbol(top[left]) || IsSymbol(top[left + 1]) || IsSymbol(top[right - 2]) || IsSymbol(top[right -1]);
-        var adjB = IsSymbol(bottom[left]) || IsSymbol(bottom[left + 1]) || IsSymbol(bottom[right - 2]) || IsSymbol(bottom[right -1]);
-        var adjM = IsSymbol(middle[left]) || IsSymbol(middle[right - 1]);
-
         if (adj)
         {
             parts.Add(match.Value);
@@ -62,4 +79,11 @@ static int[] GetEngineParts(string top, string middle, string bottom)
 static bool IsSymbol(char chr)
 {
     return !(Char.IsDigit(chr) || chr == '.');
+}
+
+static void AddAdjacents(MatchCollection numMatches, Match match, List<int> adjacent)
+{
+    adjacent.AddRange(from Match num in numMatches
+                      where (num.Index <= match.Index && match.Index <= num.Index + num.Length) || match.Index + 1 == num.Index
+                      select int.Parse(num.Value));
 }
