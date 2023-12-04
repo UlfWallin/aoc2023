79ff02e144c8912ce3b42eb94719deddf5b264f0
UlfWallin <herr.wallin@gmail.com>
2023-12-04 20:31:59 +0100

    Simplified

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,11 +1,10 @@
 ﻿using System.Text.RegularExpressions;
-using System.Linq;
 
 int sum = 0;
 int sumRatio = 0;
 List<string> lines = [];
-Regex regex = new Regex("\\*");
-Regex numRegex = new Regex("\\d+");
+Regex asteriskRegex = new("\\*");
+Regex numRegex = new("\\d+");
 
 using var reader = new StreamReader("input.txt");
 while (reader.Peek() >= 0)
@@ -19,16 +18,16 @@ lines.Add(new string('.', lines[0].Length));
 
 for (int il = 0; il < lines.Count - 2; il++)
 {
-    var matches = regex.Matches(lines[il + 1]);
+    var asterisks = asteriskRegex.Matches(lines[il + 1]);
     var numsTop = numRegex.Matches(lines[il]);
     var numsBottom = numRegex.Matches(lines[il + 2]);
     var numsCenter = numRegex.Matches(lines[il + 1]);
-    foreach(Match match in matches)
+    foreach(Match asterisk in asterisks)
     {
         List<int> adjacent = [];
-        AddAdjacents(numsTop, match, adjacent);
-        AddAdjacents(numsBottom, match, adjacent);
-        AddAdjacents(numsCenter, match, adjacent);
+        AddAdjacents(numsTop, asterisk.Index, adjacent);
+        AddAdjacents(numsBottom, asterisk.Index, adjacent);
+        AddAdjacents(numsCenter, asterisk.Index, adjacent);
 
         if (adjacent.Count == 2)
         {
@@ -43,47 +42,31 @@ for (int il = 0; il < lines.Count - 2; il++)
 Console.WriteLine(sum);
 Console.WriteLine(sumRatio);
 
-static int[] GetEngineParts(string top, string middle, string bottom)
+int[] GetEngineParts(string top, string middle, string bottom)
 {
     List<string> parts = [];
-    var matches = Regex.Matches(middle, @"\d+");
 
-    foreach (Match match in matches)
+    foreach (Match match in numRegex.Matches(middle))
     {
         var left = Math.Max(match.Index - 1, 0);
         var right = Math.Min(match.Index + match.Length + 1, middle.Length - 1);
 
-        bool adj = false;
-
-        for (int i = left; i < right; i++)
-        {
-            if (IsSymbol(top[i]) || IsSymbol(bottom[i]))
-            {
-                adj = true;
-                break;
-            }
-        }
-        if (IsSymbol(middle[left]) || IsSymbol(middle[right - 1]))
-        {
-            adj = true;
-        }
-
-        if (adj)
+        if (middle.AsEnumerable().Skip(left).Take(right - left).Any(c => IsSymbol(c)) ||
+            top.AsEnumerable().Skip(left).Take(right - left).Any(c => IsSymbol(c)) ||
+            bottom.AsEnumerable().Skip(left).Take(right - left).Any(c => IsSymbol(c)))
         {
             parts.Add(match.Value);
         }
     }
+
     return parts.Select(p => int.Parse(p)).ToArray();
 }
 
-static bool IsSymbol(char chr)
-{
-    return !(Char.IsDigit(chr) || chr == '.');
-}
+static bool IsSymbol(char chr) => !(Char.IsDigit(chr) || chr == '.');
 
-static void AddAdjacents(MatchCollection numMatches, Match match, List<int> adjacent)
+static void AddAdjacents(MatchCollection numbers, int index, List<int> adjacent)
 {
-    adjacent.AddRange(from Match num in numMatches
-                      where (num.Index <= match.Index && match.Index <= num.Index + num.Length) || match.Index + 1 == num.Index
+    adjacent.AddRange(from Match num in numbers
+                      where (num.Index <= index && index <= num.Index + num.Length) || index + 1 == num.Index
                       select int.Parse(num.Value));
 }
