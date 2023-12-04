a948e8ca62859cc6ac8d9ea3cfdf2585ab95e174
UlfWallin <herr.wallin@gmail.com>
2023-12-04 20:47:31 +0100

    Simplified day 4

--- 04/Program.cs
+++ 04/Program.cs
@@ -1,33 +1,30 @@
 ﻿using System.Text.RegularExpressions;
 var sumPoints = 0;
 var regex = new Regex(@"\d+");
-var lineNumber = 0; 
-int[] copies = new int[220];
+var cardNumber = 0; 
+int[] copies = new int[202];
 
 using var reader = new StreamReader("input.txt");
 while (reader.Peek() >= 0)
 {
     var line = reader.ReadLine() ?? "";
-
-    var card = line.Split(':')[0][5..].Trim();
-    var nums = line.Substring(line.IndexOf(':')).Split('|');
+    var nums = line[line.IndexOf(':')..].Split('|');
 
     var winningNumbers = regex.Matches(nums[0]).Select(m => int.Parse(m.Value)).ToArray();
     var holdingNumbers = regex.Matches(nums[1]).Select(m => int.Parse(m.Value)).ToArray();
 
     var wins = holdingNumbers.Intersect(winningNumbers).Count();
     var points = wins > 1 ? (int)Math.Pow(2, wins - 1) : wins;
-    copies[lineNumber] += 1;
+    copies[cardNumber]++;
     if (wins > 0) {
         for(var i = 0; i < wins; i++) {
-            var wonCard = i + lineNumber + 1;
-            copies[wonCard] += copies[lineNumber];
+            var wonCard = i + cardNumber + 1;
+            copies[wonCard] += copies[cardNumber];
         }
     }
 
-    //Console.WriteLine($"Card {card}: {points}");
     sumPoints += points;
-    lineNumber++;
+    cardNumber++;
 }
 Console.WriteLine($"Sum: {sumPoints}");
 Console.WriteLine($"Cards: {copies.Sum()}");
