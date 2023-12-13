8d31b5a68fb27136dee645ff10114e8e5f473510
UlfWallin <herr.wallin@gmail.com>
2023-12-13 15:07:26 +0100

    09, part two

--- 09/Program.cs
+++ 09/Program.cs
@@ -1,13 +1,22 @@
 ﻿long sum = 0;
+long sum2 = 0;
 using var reader = new StreamReader("input.txt");
 while (reader.Peek() >= 0)
 {
+    Stack<long> stack = [];
     var line = reader.ReadLine().Split(' ').Select(m => long.Parse(m)).ToArray();
     sum += line[^1];
+    stack.Push(line[0]);
     while(line.Sum() != 0) {
         line = line[1..].Zip(line, (x, y) => x - y).ToArray();
+        stack.Push(line[0]);
         sum += line[^1];
     }
+    var p2 = stack.Pop();
+    while(stack.Count > 0){
+        p2 = stack.Pop() - p2;
+    }
+    sum2 += p2;
 }
 Console.WriteLine(sum);
-
+Console.WriteLine(sum2);
